c13ef0b37c98c20877ee44fcc7b710822d489be9
arunvinit <74286546+arunvinit@users.noreply.github.com>
2024-10-12 13:18:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ console.log(process.env.SECRET);
 
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const mongoose = require('mongoose');
 const {storage}=require("./cloudinary")
 const multer  = require('multer')
